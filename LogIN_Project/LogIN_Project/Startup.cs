33c266378a0ddb76fe73d3f208b5fe3e04dc49aa
tim811223 <nahaf25@gmail.com>
2018-12-21 17:14:29 +0800

    原始專案

---
+++ LogIN_Project/LogIN_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LogIN_Project.Startup))]
+namespace LogIN_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
