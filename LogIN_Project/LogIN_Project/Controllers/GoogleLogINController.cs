9672826d3487130624618fe3c01968550e155fc2
tim811223 <nahaf25@gmail.com>
2018-12-21 17:35:31 +0800

    加入Google登入頁面

---
+++ LogIN_Project/LogIN_Project/Controllers/GoogleLogINController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LogIN_Project.Controllers
+{
+    public class GoogleLogINController : Controller
+    {
+        // GET: GoogleLogIN
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
